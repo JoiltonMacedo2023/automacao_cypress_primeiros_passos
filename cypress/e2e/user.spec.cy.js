5abb8dae643b4d4dba1e70972a762e1a3c7c4d0d
Joilton Macedo <joiltonmacedo2022@outlook.com>
2024-05-19 21:41:12 -0300

    feat: User select field combox add

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -10,14 +10,17 @@ describe('Orange HRM Tests', () => {
     dashboradGrid: ".orangehrm-dashboard-grid",
     credentialAlert: "[role='alert']",
     myInfoButton: "[href='/web/index.php/pim/viewMyDetails']",
-    firstNameField:"[name='firstName']",
-    lastNameField:"[name='lastName']",
+    firstNameField: "[name='firstName']",
+    middleNameField: "[name='middleName']",
+    lastNameField: "[name='lastName']",
     genericField: ".oxd-input--active",
     dateField: "[placeholder='yyyy-dd-mm']",
     dateCloseButton: ".--close",
-    submitButton:"[type='submit']"    
+    selectField: ".oxd-select-text--arrow",
+    selectGenderField: '.oxd-radio-input',
+    submitButton: "[type='submit']",
   }
-    
+
   it.only('User Info Update - Success', () => {
     cy.visit('/auth/login')
     cy.get(selectorsList.usernameField).type(userData.userSucess.username)
@@ -26,15 +29,22 @@ describe('Orange HRM Tests', () => {
     cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
     cy.get(selectorsList.dashboradGrid)
     cy.get(selectorsList.myInfoButton).click()
-    cy.get(selectorsList.firstNameField).clear().type('firstTest')
-    cy.get(selectorsList.lastNameField).clear().type('lastTest') 
+    cy.get(selectorsList.firstNameField).clear().type('Ton')
+    cy.get(selectorsList.middleNameField).clear().type('Maxuel')
+    cy.get(selectorsList.lastNameField).clear().type('Macedo')
     cy.get(selectorsList.genericField).eq(3).clear().type('employ')
     cy.get(selectorsList.genericField).eq(4).clear().type('OtherIdTest')
     cy.get(selectorsList.genericField).eq(5).clear().type('Lincensetest')
-    cy.get(selectorsList.dateField).eq(1).clear().type('2024-08-30')
+    cy.get(selectorsList.dateField).eq(0).clear().type('2024-05-13')
     cy.get(selectorsList.dateCloseButton).click()
-    cy.get(selectorsList.submitButton).eq(0).click()
-    cy.get('body').should('contain', 'Successfully Updated')  
+    cy.get(selectorsList.selectField).eq(0).click({ force: true })
+    cy.get(':nth-child(27) > span').click()
+    cy.get(selectorsList.selectField).eq(1).click({ force: true })
+    cy.get(':nth-child(3) > span').click()
+    cy.get(selectorsList.dateField).eq(1).clear().type('1981-08-30')
+    cy.get(selectorsList.selectGenderField).eq(2).click()
+    cy.get(selectorsList.submitButton).eq(0).click({ force: true })
+    cy.get('body').should('contain', 'Successfully Updated')
     cy.get('.oxd-toast-close')
   })
 
@@ -44,6 +54,6 @@ describe('Orange HRM Tests', () => {
     cy.get(selectorsList.passwordField).type(userData.userFail.password)
     cy.get(selectorsList.loginButton).click()
     cy.get(selectorsList.credentialAlert)
-  
-  })  
+
+  })
 })
