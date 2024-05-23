227ea7b94b16e68044c216d180319b8b3741acb4
Joilton Macedo <joiltonmacedo2022@outlook.com>
2024-05-22 22:07:31 -0300

    refactor: Create Pages Objets login

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -1,14 +1,13 @@
 import userData from '../fixtures/users/userData.json'
+import LoginPage from '../pages/loginPage.js'
+
+const loginPage = new LoginPage()
 
 describe('Orange HRM Tests', () => {
 
-  const selectorsList = {
-    usernameField: "[name='username']",
-    passwordField: "[name='password']",
-    loginButton: "[type='submit']",
+  const selectorsList = {   
     sectionTitleTopBar: '.oxd-topbar-header-breadcrumb-module',
-    dashboradGrid: ".orangehrm-dashboard-grid",
-    credentialAlert: "[role='alert']",
+    dashboradGrid: ".orangehrm-dashboard-grid",    
     myInfoButton: "[href='/web/index.php/pim/viewMyDetails']",
     firstNameField: "[name='firstName']",
     middleNameField: "[name='middleName']",
@@ -17,15 +16,14 @@ describe('Orange HRM Tests', () => {
     dateField: "[placeholder='yyyy-dd-mm']",
     dateCloseButton: ".--close",
     selectField: ".oxd-select-text--arrow",
-    selectGenderField: '.oxd-radio-input',
-    submitButton: "[type='submit']",
+    selectGenderField: '.oxd-radio-wrapper',
+    submitButton: "[type='submit']",    
   }
 
   it.only('User Info Update - Success', () => {
-    cy.visit('/auth/login')
-    cy.get(selectorsList.usernameField).type(userData.userSucess.username)
-    cy.get(selectorsList.passwordField).type(userData.userSucess.password)
-    cy.get(selectorsList.loginButton).click()
+    loginPage.accessLoginPage()
+    loginPage.loginWithUser(userData.userSucess.username, userData.userSucess.password ) 
+      
     cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
     cy.get(selectorsList.dashboradGrid)
     cy.get(selectorsList.myInfoButton).click()
@@ -42,10 +40,10 @@ describe('Orange HRM Tests', () => {
     cy.get(selectorsList.selectField).eq(1).click({ force: true })
     cy.get(':nth-child(3) > span').click()
     cy.get(selectorsList.dateField).eq(1).clear().type('1981-08-30')
-    cy.get(selectorsList.selectGenderField).eq(2).click()
+    cy.get(selectorsList.selectGenderField).eq(1).click()   
     cy.get(selectorsList.submitButton).eq(0).click({ force: true })
     cy.get('body').should('contain', 'Successfully Updated')
-    cy.get('.oxd-toast-close')
+    cy.get('.oxd-toast-close')    
   })
 
   it('Login - Fail', () => {
