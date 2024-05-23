f0453235bdfd33c7cc8262da845388fdc20e9072
Joilton Macedo <joiltonmacedo2022@outlook.com>
2024-05-23 14:04:32 -0300

    perf:Add page objects: menu page

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -1,19 +1,21 @@
 import userData from '../fixtures/users/userData.json'
-import LoginPage from '../pages/loginPage.js'
+import LoginPage from '../pages/loginPage'
+import DashboardPage from '../pages/dashboardPage'
+import MenuPage from '../pages/menuPage'
 
 const loginPage = new LoginPage()
+const dashboardPage = new DashboardPage()
+const menuPage = new MenuPage()
 
 describe('Orange HRM Tests', () => {
 
-  const selectorsList = {   
-    sectionTitleTopBar: '.oxd-topbar-header-breadcrumb-module',
-    dashboradGrid: ".orangehrm-dashboard-grid",    
-    myInfoButton: "[href='/web/index.php/pim/viewMyDetails']",
+  const selectorsList = {          
+    
     firstNameField: "[name='firstName']",
     middleNameField: "[name='middleName']",
     lastNameField: "[name='lastName']",
     genericField: ".oxd-input--active",
-    dateField: "[placeholder='yyyy-dd-mm']",
+    dateField: ".oxd-date-input",
     dateCloseButton: ".--close",
     selectField: ".oxd-select-text--arrow",
     selectGenderField: '.oxd-radio-wrapper',
@@ -22,24 +24,25 @@ describe('Orange HRM Tests', () => {
 
   it.only('User Info Update - Success', () => {
     loginPage.accessLoginPage()
-    loginPage.loginWithUser(userData.userSucess.username, userData.userSucess.password ) 
-      
-    cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
-    cy.get(selectorsList.dashboradGrid)
-    cy.get(selectorsList.myInfoButton).click()
+    loginPage.loginWithAnyUser(userData.userSucess.username, userData.userSucess.password ) 
+    
+    dashboardPage.checkDashboardPage()
+    menuPage.accessMyInfo()
+    
     cy.get(selectorsList.firstNameField).clear().type('Ton')
     cy.get(selectorsList.middleNameField).clear().type('Maxuel')
     cy.get(selectorsList.lastNameField).clear().type('Macedo')
     cy.get(selectorsList.genericField).eq(3).clear().type('employ')
     cy.get(selectorsList.genericField).eq(4).clear().type('OtherIdTest')
     cy.get(selectorsList.genericField).eq(5).clear().type('Lincensetest')
-    cy.get(selectorsList.dateField).eq(0).clear().type('2024-05-13')
-    cy.get(selectorsList.dateCloseButton).click()
+    cy.get(selectorsList.dateField).eq(0).clear().type('2023-04-13')
+    cy.get(selectorsList.dateCloseButton).eq(0).click()
+    cy.get(selectorsList.genericField).eq(7).clear().type('sinNumber')
     cy.get(selectorsList.selectField).eq(0).click({ force: true })
     cy.get(':nth-child(27) > span').click()
     cy.get(selectorsList.selectField).eq(1).click({ force: true })
     cy.get(':nth-child(3) > span').click()
-    cy.get(selectorsList.dateField).eq(1).clear().type('1981-08-30')
+    cy.get(selectorsList.dateField).eq(1).type('2020-10-10')   
     cy.get(selectorsList.selectGenderField).eq(1).click()   
     cy.get(selectorsList.submitButton).eq(0).click({ force: true })
     cy.get('body').should('contain', 'Successfully Updated')
