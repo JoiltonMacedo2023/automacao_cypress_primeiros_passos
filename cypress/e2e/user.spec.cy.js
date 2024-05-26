f55fddebb2cb4d79a298027fab96bd7dfa088a0b
Joilton Macedo <joiltonmacedo2022@outlook.com>
2024-05-26 16:47:15 -0300

    perf: add ramdom values on user spec chancejs

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -4,6 +4,9 @@ import DashboardPage from '../pages/dashboardPage'
 import MenuPage from '../pages/menuPage'
 import MyInfoPage from '../pages/myInfoPage'
 
+const Chance = require('chance')
+
+const chance = new Chance()
 const loginPage = new LoginPage()
 const dashboardPage = new DashboardPage()
 const menuPage = new MenuPage()
@@ -11,27 +14,22 @@ const myInfoPage = new MyInfoPage()
 
 describe('Orange HRM Tests', () => { 
 
-  it.only('User Info Update - Success', () => {
+  it('User Info Update - Success', () => {
     loginPage.accessLoginPage()
-    loginPage.loginWithAnyUser(userData.userSucess.username, userData.userSucess.password ) 
+    loginPage.loginWithAnyUser(userData.userSucess.username, userData.userSucess.password) 
     
     dashboardPage.checkDashboardPage()
 
     menuPage.accessMyInfo()
 
-    myInfoPage.fillPersonalDetails('Ton', 'Maxuel', 'Macedo')
+    myInfoPage.fillPersonalDetails(
+      chance.first({ nationality: "it"} ),
+      chance.last({ nationality: "it"} ),
+      chance.last({ nationality: "it"})
+    )
     myInfoPage.fillEmployeeDetails('employId', 'OtherId', '12345', '2025-07-22')
     myInfoPage.fillStatus('1981-08-30')
     myInfoPage.saveForm()     
-     
   })
 
-  it('Login - Fail', () => {
-    cy.visit('/auth/login')
-    cy.get(selectorsList.usernameField).type(userData.userFail.username)
-    cy.get(selectorsList.passwordField).type(userData.userFail.password)
-    cy.get(selectorsList.loginButton).click()
-    cy.get(selectorsList.credentialAlert)
-
-  })
 })
